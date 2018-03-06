ce2ecd8b186ac2412e6dc4edf3b9d0a61759b865
andrewmahoneyf <andrew.mahoney.f@gmail.com>
2018-03-05 17:55:40 -0800

    Update data processing

--- scripts/process_data.R
+++ scripts/process_data.R
@@ -1,12 +1,12 @@
-## File used for processing both data sets
-install.packages(c("ggplot2", "data.table", "dplyr", "devtools"))
+## File used for processing data sets
+# install.packages(c("data.table", "dplyr"))
 library(data.table)
 library(dplyr)
-library(ggplot2)
 
 ## import static files
-system.time(crypto_data <- fread("data/crypto_history.bz2"))
-# system.time(twitter_data <- fread("data/twitter_history.csv"))
+system.time(crypto_data <- fread("../data/crypto_history.bz2"))
+system.time(news_data <- fread("../data/201718-filtered-news-complete.csv"))
+system.time(google_data <- fread("../data/google_trends.csv"))
 
 # Manipulate both data sets for desired use and compatibility
 crypto_data <- crypto_data %>% filter(substr(date, 0, 4) == "2017" | substr(date, 0, 4) == "2018")
@@ -16,7 +16,8 @@ top5 <- crypto_data %>% filter(ranknow <= 5)
 
 # Write function to take in the crypto data set and string parameter
 # and output a data frame for the specified cryptocurrency
-ethereum <- crypto_data %>% filter(symbol == "ETC")
+temp <- top5 %>% group_by(name) %>% summarise(Days = n())
+ethereum <- crypto_data %>% filter(symbol == "ETH")
 
 # Write a function to maniputale the crypto data and create a seperate
 # data frame with weekly averages instead of daily
